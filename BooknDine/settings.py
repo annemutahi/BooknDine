2d98b0a8ec7b076552d324e58b49dfb859ed13c6
annemutahi <ms.nyamburamutahi@gmail.com>
2025-10-23 09:17:03 +0300

    changed debug to false

--- BooknDine/settings.py
+++ BooknDine/settings.py
@@ -27,10 +27,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j#u79#jlfplvrk&g)&w%ls8&#s%mhrs)+n4*ixf_!4*&&38_(c'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
